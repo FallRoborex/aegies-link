06d9905f5ee9fa974ec98dc1cc2a0cbfb6f3766b
FallRoborex <robertoramirezgalan04@gmail.com>
2026-05-08 23:12:38 -0400

    Feat: basic UDP server with echo and ACK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() {
+    let socket = UdpSocket::bind("0.0.0.0:7878").await.unwrap();
+    println!("Aegis-link server listening on port 7878"); 
+
+    let mut buf = [0u8; 1024];
+
+    loop {
+        let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
+        let msg = String::from_utf8_lossy(&buf[..len]);
+        println!("Reveived from {}: {}", addr, msg);
+
+        socket.send_to(b"ACK", addr).await.unwrap();
+    }
+
 }
